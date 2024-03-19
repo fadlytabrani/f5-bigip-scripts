a8f46894ce2085d8c5cba742c92d155b9163c544
fadlytabrani <fadly.tabrani@gmail.com>
2024-03-20 03:13:16 +1000

    Update qkview-ihealth.py
    Corrected reverse iteration of entries from file. Added inline documentation and default logging.

--- qkview-ihealth/qkview-ihealth.py
+++ qkview-ihealth/qkview-ihealth.py
@@ -10,6 +10,7 @@ It reads api credentials from a file named ihealth.apitokens in its current path
 Each credential is a line in the format [client_id]:[client_secret].
 """
 
+import logging
 import os
 import requests
 import subprocess
@@ -21,6 +22,7 @@ qkview_url = 'https://ihealth2-api.f5.com/qkview-analyzer/api/qkviews'
 upload_url = qkview_url + '?visible_in_gui=true'
 
 qkview_file = datetime.now().strftime('%Y%m%d-%H%M') + '.qkview'
+
 apitokens_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ihealth.apitokens')
 access_token = ''
 
@@ -29,7 +31,7 @@ with open(apitokens_file, 'r') as _f:
     # Read the apitokens file from the bottom up.
     api_keys = _f.read().split()
 
-for api_key in api_keys:
+for api_key in reversed(api_keys):
 
     # Extract the client id and secret, get access token.
     client_id, key = api_key.split(':')
@@ -42,20 +44,34 @@ for api_key in api_keys:
         access_token = response.json()['access_token']
         break
 
-# Test access to the API before proceeding
+# Exit immediately if no valid keys are found.
+if not access_token:
+    logging.critical('No valid keys found, might be expired.')
+    exit(1)
+
+# Test access to the API before proceeding.
 response = requests.get(qkview_url,
                         headers={'Authorization': 'Bearer ' + access_token})
 if response.status_code != 200:
+    logging.error('iHealth test failed, site might be down.')
     exit(1)
 
 cmd = ['nice', '-n', '19', 'qkview', '-f', qkview_file]
 
-# Exit immediately with error code 1 if qkview creation failed.
-if subprocess.call(cmd) != 0:
-    exit(1)
+# Exit immediately with error code if qkview creation failed.
+return_code = subprocess.call(cmd)
+if return_code != 0:
+    logging.critical('QKVIEW file could not be created.')
+    exit(return_code)
 
+# The qkview file will be created in /var/tmp. Use curl to upload it to iHealth.
 cmd = ['curl', '--location', upload_url, '--header', 'Authorization: Bearer ' + access_token,
        '--form', 'qkview=@"' + '/var/tmp/' + qkview_file + '"']
 
 # Exit with curl return code.
-exit(subprocess.call(cmd))
+return_code = subprocess.call(cmd)
+if return_code != 0:
+    logging.critical("QKVIEW file %s upload failed." % (qkview_file))
+    exit(return_code)
+else:
+    logging.info("QKVIEW file %s upload successful." % (qkview_file))
